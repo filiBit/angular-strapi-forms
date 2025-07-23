79cd902660b6af8dd76c9f35fccc8712fad19c70
Filip Biterski <filip.biterski@filibit.com>
2025-07-23 19:11:55 +0200

    refactor: auth-service

--- packages/app/src/services/auth.service.ts
+++ packages/app/src/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "../environments/environment";
 import { User } from "../app/types/user";
+import { StrapiErrorResponse } from "../app/types/strapi-error-response";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
@@ -10,18 +11,42 @@ export class AuthService {
     jwt: null | string = null;
     user: null | User = null;
 
-    async login(
+    get isLoggedIn(): boolean {
+        return !!this.jwt && !!this.user;
+    }
+
+    login(
         { identifier, password }: { identifier: string; password: string },
     ) {
-        return this.http.post<{ jwt: string; user: User }>(
+        this.jwt = null;
+        this.user = null;
+
+        return fetch(
             `${environment.strapiUrl}/auth/local`,
             {
-                identifier,
-                password,
+                method: "post",
+                headers: [["content-type", "application/json"]],
+                body: JSON.stringify({
+                    identifier,
+                    password,
+                }),
             },
-        ).subscribe((b) => {
-            this.jwt = b.jwt;
-            this.user = b.user;
-        });
+        ).then(async (res) => {
+            if (res.status !== 200) {
+                this.jwt = null;
+                this.user = null;
+                throw await res.json();
+            }
+            return res.json() as Promise<{ jwt: string; user: User }>;
+        })
+            .then((body) => {
+                this.jwt = body.jwt;
+                this.user = body.user;
+                return null;
+            }).catch((err: StrapiErrorResponse) => {
+                throw new Error(
+                    err.error.details?.errors?.[0].message || err.error.message,
+                );
+            });
     }
 }
