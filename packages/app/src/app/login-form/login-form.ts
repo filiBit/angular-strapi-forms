8ba623c9c52ad8bcb10a343257315e131c2300e8
Filip Biterski <filip.biterski@filibit.com>
2025-07-28 05:32:31 +0200

    feat(app): login-form

---
+++ packages/app/src/app/login-form/login-form.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatFormField } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { StateService } from "../../services/state.service";
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+    selector: "login-form",
+    imports: [FormsModule, MatFormField, MatInputModule, MatButtonModule],
+    templateUrl: "./login-form.html",
+})
+export class LoginForm {
+    stateService = inject(StateService);
+    authService = inject(AuthService);
+
+    identifier = "";
+    password = "";
+    errorMessage: null | string = null;
+
+    async login(event: Event) {
+        event.preventDefault();
+        this.errorMessage = null;
+        await this.authService.login({
+            identifier: this.identifier,
+            password: this.password,
+        }).catch((err: Error) => {
+            console.log(err.message);
+            this.errorMessage = err.message;
+        });
+
+        this.stateService.initialize();
+    }
+}
