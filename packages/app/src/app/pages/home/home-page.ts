c4714ac4f152db9541c7a64b90ea132a8481729e
Filip Biterski <filip.biterski@filibit.com>
2025-07-28 14:14:48 +0200

    refactor(app): render components, update style

--- packages/app/src/app/pages/home/home-page.ts
+++ packages/app/src/app/pages/home/home-page.ts
@@ -2,11 +2,14 @@ import { Component, inject } from "@angular/core";
 import { AuthService } from "../../../services/auth.service";
 import { LoginForm } from "../../login-form/login-form";
 import { StateService } from "../../../services/state.service";
+import { FormGroups } from "../../form-groups/form-groups";
 
 @Component({
     selector: "home-page",
     templateUrl: "./home-page.html",
-    imports: [LoginForm],
+    imports: [LoginForm, FormGroups],
+    styles:
+        `:host {width: 100%; display: flex; flex-direction: column; align-items: center;}`,
 })
 export class HomePage {
     authService = inject(AuthService);
