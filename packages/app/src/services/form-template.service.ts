a4bb1c8c972e0c6b50388339438680628722524d
Filip Biterski <filip.biterski@filibit.com>
2025-07-28 04:53:43 +0200

    feat(app): form-template service

--- packages/app/src/services/form-template.service.ts
+++ packages/app/src/services/form-template.service.ts
@@ -1,6 +1,42 @@
 import { inject, Injectable } from "@angular/core";
 import { environment } from "../environments/environment";
 import { AuthService } from "./auth.service";
+import { StrapiResponse } from "../types/strapi-response";
+
+export interface FormTemplateDTO {
+    id: number;
+    documentId: string;
+    templateCode: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    version: number;
+    mode: string;
+    groupType: {
+        documentId: string;
+        name: string;
+    };
+    isActive: boolean;
+    validFrom: string;
+    validTo: string;
+    fields: { [key: string]: unknown };
+}
+
+export interface FormTemplate {
+    id: number;
+    documentId: string;
+    templateCode: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    version: number;
+    mode: string;
+    groupType: string;
+    isActive: boolean;
+    validFrom: string;
+    validTo: string;
+    fields: { [key: string]: unknown };
+}
 
 @Injectable({
     providedIn: "root",
@@ -8,11 +44,44 @@ import { AuthService } from "./auth.service";
 export class FormTemplateService {
     authService = inject(AuthService);
 
-    getFormTemplates() {
-        return fetch(`${environment.strapiUrl}/form-templates`, {
-            headers: [
-                ["Authorization", `Bearer ${this.authService.jwt}`],
-            ],
-        }).then((res) => res.json());
+    async getFormTemplates(): Promise<FormTemplate[]> {
+        let nextPage = 1;
+        const result: FormTemplate[] = [];
+
+        do {
+            await fetch(
+                `${environment.strapiUrl}/form-templates?populate[groupType][fields][0]=name&pagination[page]=${nextPage}`,
+                {
+                    headers: [
+                        ["Authorization", `Bearer ${this.authService.jwt}`],
+                    ],
+                },
+            ).then((res) =>
+                res.json() as unknown as StrapiResponse<FormTemplateDTO>
+            ).then((b) => {
+                result.push(
+                    ...b.data.map((ft) => ({
+                        ...ft,
+                        groupType: ft.groupType.name,
+                    })),
+                );
+                nextPage = b.meta.pagination.page < b.meta.pagination.pageCount
+                    ? nextPage + 1
+                    : 0;
+            });
+        } while (nextPage > 0);
+
+        return result;
+    }
+
+    async createFormTemplate() {
+        await fetch(
+            `${environment.strapiUrl}/form-templates`,
+            {
+                headers: [
+                    ["Authorization", `Bearer ${this.authService.jwt}`],
+                ],
+            },
+        );
     }
 }
