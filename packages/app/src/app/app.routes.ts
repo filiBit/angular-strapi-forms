0946fac6e963e1169581ee7583e6c8c8a1fd5f49
Filip Biterski <filip.biterski@filibit.com>
2025-07-31 00:37:01 +0200

    feat(app): not-found page

--- packages/app/src/app/app.routes.ts
+++ packages/app/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from "@angular/router";
 import { HomePage } from "./pages/home/home-page";
+import { NotFound } from "./not-found/not-found";
 
 export const routes: Routes = [{
     path: "",
     component: HomePage,
+}, {
+    path: "**",
+    component: NotFound,
 }];
