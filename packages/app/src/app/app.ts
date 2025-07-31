a96d612474355d7b6601fd26ac37f0c5240f7cde
Filip Biterski <filip.biterski@filibit.com>
2025-07-31 19:21:38 +0200

    feat(app): add layout header

--- packages/app/src/app/app.ts
+++ packages/app/src/app/app.ts
@@ -1,13 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { StateService } from "../services/state.service";
 
 @Component({
     selector: "app-root",
     imports: [
         RouterOutlet,
+        MatButtonModule,
+        MatIconModule,
     ],
     templateUrl: "./app.html",
     styleUrl: "./app.css",
 })
 export class App {
+    stateService = inject(StateService);
+
+    user = computed(() => this.stateService.getAuth()?.user);
 }
