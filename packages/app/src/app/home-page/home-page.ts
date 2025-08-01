3b1d7a4651bb11ba7cd805eafe8feb0888aa9966
Filip Biterski <filip.biterski@filibit.com>
2025-08-01 04:47:48 +0200

    fix(app): home page

--- packages/app/src/app/home-page/home-page.ts
+++ packages/app/src/app/home-page/home-page.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from "@angular/core";
-import { AuthService } from "../../../services/auth.service";
-import { LoginForm } from "../../login-form/login-form";
-import { StateService } from "../../../services/state.service";
-import { FormGroups } from "../../form-groups/form-groups";
-import { LayoutNarrow } from "../../layout-narrow/layout-narrow";
+import { AuthService } from "../../services/auth.service";
+import { LoginForm } from "../login-form/login-form";
+import { StateService } from "../../services/state.service";
+import { FormGroups } from "../form-groups/form-groups";
+import { LayoutNarrow } from "../layout-narrow/layout-narrow";
 
 @Component({
     selector: "home-page",
